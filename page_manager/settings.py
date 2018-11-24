69f6da269ad19a4db447fe724e2d127bc8ed8907
Salabh A N <salabhn@gmail.com>
2018-11-24 22:52:14 +0400

    changes

--- page_manager/settings.py
+++ page_manager/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -151,7 +150,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = ( os.path.join(PROJECT_DIR, 'static'), )
 
 
-STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 LOGIN_URL = 'login'
